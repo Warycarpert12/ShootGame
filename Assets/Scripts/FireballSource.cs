920cd0f117ff7b2e3e1f7ec3a87b75a02af82872
Warycarpert12 <artemg655@gmail.com>
2024-03-04 14:15:29 +0300

    Что-то как-то работает

--- Assets/Scripts/FireballSource.cs
+++ Assets/Scripts/FireballSource.cs
@@ -11,12 +11,16 @@ public class FireballSource : MonoBehaviour
 
     void Update()
     {
-        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0));
 
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
             targetPoint.position = hit.point;
+        }
+        else
+        {
+            targetPoint.position = Camera.main.transform.position + ray.direction * 100;
         }
 
         transform.LookAt(targetPoint.position);
