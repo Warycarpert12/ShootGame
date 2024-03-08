212a450613ddde1a7fe371700ae1e0227765828f
Warycarpert12 <artemg655@gmail.com>
2024-03-08 21:36:09 +0300

    Add kit

---
+++ Assets/Scripts/Addkit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addkit : MonoBehaviour
+{
+    public float healAmount = 50;
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+        if (playerHealth != null )
+        {
+            playerHealth.AddHealth( healAmount );
+        }
+        Destroy(gameObject);
+
+    }
+    
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -30,6 +30,12 @@ public class PlayerHealth : MonoBehaviour
         DrawHealthBar();
 
     }
+    public void AddHealth(float amount)
+    {
+        value += amount;
+        value = Mathf.Clamp(value, 0, _maxValue);
+        DrawHealthBar();
+    }
     private void PlayerIsdead()
     {
             gameplayUI.SetActive(false);
