890e21d5fa9e45d954b4881a7e1e479e7974c2d7
Warycarpert12 <artemg655@gmail.com>
2024-03-09 23:39:16 +0300

    Add explosion damage

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -6,6 +6,7 @@ public class Explosion : MonoBehaviour
 {
     public float maxSize = 5;
     public float Speed = 1;
+    public float damage = 50;
     void Start()
     {
         transform.localScale = Vector3.zero;
@@ -18,5 +19,20 @@ public class Explosion : MonoBehaviour
         {
             Destroy(gameObject);
         }    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.DealDamage(damage);
+        }
+        
+        var enemyHealth = other.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
     }
 }
